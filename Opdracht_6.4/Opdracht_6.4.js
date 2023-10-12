549e350708a7d6d18072870961b73ca3436c7290
9022624 <144020444+9022624@users.noreply.github.com>
2023-10-12 17:14:49 +0200

    6.4

---
+++ Opdracht_6.4/Opdracht_6.4.js
@@ -0,0 +1,27 @@
+
+/* 6.4 oefening 1 */
+
+function Calculate(getal1, getal2)
+{
+antwoord = getal1 + getal2;
+console.log("Het antwoord van de som is: ");
+console.log(antwoord);
+}
+
+/* 6.4 oefening 2 */
+
+function multiplyByFive(getal1)
+{
+antwoord = getal1 * 5;
+console.log("Het antwoord van de som is: ");
+console.log(antwoord);
+}
+
+/* 6.4 oefening 3 */
+
+function calculateMinutes(seconden)
+{
+antwoord = seconden / 60;
+console.log("Het antwoord van de som is: ");
+console.log(antwoord);
+}
